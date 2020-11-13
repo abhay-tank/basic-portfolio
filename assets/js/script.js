9a1d5e330d5256a4cff50e9e4e608f43b8f74e52
abhay-tank <abhay.tank@raweng.com>
2020-11-14 00:20:16 +0530

    Changed project img height and removed console logs

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ window.onload = () => {
     activateItem = urlFound.slice(1, urlFound.length);
     activateItem = activateItem + "-link";
     activateItem = document.getElementById(activateItem);
-    console.log(activateItem);
   } else {
     navItems = document.getElementsByClassName("nav-link");
     activateItem = navItems[0];
@@ -15,15 +14,11 @@ window.onload = () => {
 };
 
 activateNavItem = (item) => {
-  console.log("clicked", item);
   navItems = document.getElementsByClassName("nav-link");
-  console.log(navItems);
   Array.from(navItems).forEach((element) => {
-    console.log(element);
     element.style.backgroundColor = "#161616";
   });
   activateItem = document.getElementById(item);
-  console.log(activateItem);
   activateItem.style.backgroundColor = "white";
   document.getElementById(item).style.opacity = 1;
   document.getElementById(item).style.filter = "grayscale(0%)";
